c500e0b70fd892e0c028caf2fc5ea2dfbec3b50b
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2019-06-04 16:59:38 -0300

    Feito a parte de conexão com o banco de dados.

---
+++ BancoDadosFarmacia/Repositorio/RemedioRepositorio.cs
@@ -0,0 +1,77 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class RemedioRepositorio
+    {
+        string CadeiadeConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\65982\Documents\ExemploBancoDados02.mdf;Integrated Security=True;Connect Timeout=30";
+
+        /*Método que irá retornar os dados 
+         * dos remédios (List<Remedio>) da tabela remedios
+         */
+
+        public List<Remedio> ObterTodos()
+        {
+
+            //Cria a conexao com o banco de dados
+
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiadeConexao;
+            conexao.Open();
+
+            // Cria o comando para ser executado no bd
+            //e diz para este comando qual é a conexao que esta 
+            //aberta 
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = "SELECT * FROM remedios";
+
+            //Cira a tabela em memória para poder obter
+            //os dados que são retornados do bd executando
+            // o comando no bd
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            //Cria uma lista de remedios para adicionar os remedios no bd 
+
+            List<Remedio> remedios = new List<Remedio>();
+
+            //Percorre todos os registros lidos do bd 
+
+            for(int i = 0; i < tabela.Rows.Count; i++)
+            {
+                DataRow linha = tabela.Rows[i];
+                //Cria um objeto com as informações
+                //obtidas no bd
+                Remedio remedio = new Remedio();
+                remedio.Id = Convert.ToInt32(linha["id"]);
+                remedio.Nome = linha["nome"].ToString();
+                remedio.Generico = Convert.ToBoolean(linha["generico"]);
+                remedio.Categoria = linha["categoria"].ToString();
+                remedio.Solido = Convert.ToBoolean(linha["solido"]);
+                remedio.ContraIndicacoes = linha["contra_indicacoes"].ToString();
+                remedio.Bula = linha["bula"].ToString();
+                remedio.Faixa = linha["faixa"].ToString();
+                remedio.PrecisaReceita = Convert.ToBoolean(linha["precisa_receita"]);
+                // Adiciona o objeto que foi criado a lista de filmes
+                remedios.Add(remedio);
+                
+            }
+            //Fecha a conexao do bd 
+            conexao.Close();
+            // Retorna a lista de filmes 
+            return remedios;
+        }
+
+
+    }
+}
